97063d20aa6d9877cf976ffda9ae9fb805b38496
Arles96 <arles.cerrato@gmail.com>
2017-02-22 15:32:06 -0600

    Se ha agregado el main

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+//Funcion que retorna un numero aleatorio
+int randomNumber();
+//Funcion que contiene los nombres de las cartas
+string* nameCards();
+//FUncion que contiene el valor de las cartas
+int* valueCards();
+
+int main ()
+{
+  string answer="s";
+  while (answer=='s'){
+    
+  }
+  return 0;
+}
+
+int randomNumber()
+{
+
+}
